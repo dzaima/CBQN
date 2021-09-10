ed2374fcc72be9da1de9e5be5f6a61e093d33954
dzaima <dzaimagit@gmail.com>
2021-09-10 21:01:31 +0300

    ∨`

--- src/builtins/md1.c
+++ src/builtins/md1.c
@@ -111,6 +111,11 @@ B scan_c1(Md1D* d, B x) { B f = d->f;
       if (xe==el_i16) { i16* xp=i16any_ptr(x); i8* rp; B r=m_i8arrv(&rp, ia); i8 c=(i8)x0; rp[0]=c; for (usz i=1; i<ia; i++) { c = c!=xp[i]; rp[i]=c; } dec(x); return r; }
       if (xe==el_i32) { i32* xp=i32any_ptr(x); i8* rp; B r=m_i8arrv(&rp, ia); i8 c=(i8)x0; rp[0]=c; for (usz i=1; i<ia; i++) { c = c!=xp[i]; rp[i]=c; } dec(x); return r; }
     }
+    if (rtid==11) { // ∨
+      if (xe==el_i8 ) { i8*  xp=i8any_ptr (x); i8* rp; B r=m_i8arrv(&rp, ia); bool c=0; for (usz i=0; i<ia; i++) { if ((xp[i]&1)!=xp[i])goto base; c|=xp[i]; rp[i]=c; } dec(x); return r; }
+      if (xe==el_i16) { i16* xp=i16any_ptr(x); i8* rp; B r=m_i8arrv(&rp, ia); bool c=0; for (usz i=0; i<ia; i++) { if ((xp[i]&1)!=xp[i])goto base; c|=xp[i]; rp[i]=c; } dec(x); return r; }
+      if (xe==el_i32) { i32* xp=i32any_ptr(x); i8* rp; B r=m_i8arrv(&rp, ia); bool c=0; for (usz i=0; i<ia; i++) { if ((xp[i]&1)!=xp[i])goto base; c|=xp[i]; rp[i]=c; } dec(x); return r; }
+    }
   }
   base:;
   
