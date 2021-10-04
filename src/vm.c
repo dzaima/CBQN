71de4fcc5932f64fd0614b3f81628d709006f8f0
dzaima <dzaimagit@gmail.com>
2021-10-04 16:19:43 +0300

    don't use x++ on bool

--- src/vm.c
+++ src/vm.c
@@ -222,7 +222,7 @@ Block* compileBlock(B block, Comp* comp, bool* bDone, u32* bc, usz bcIA, B allBl
       c = bc+idx;
       bool argUsed[6] = {0,0,0,0,0,0};
       while (*c!=RETN & *c!=RETD) {
-        if (*c==VARO | *c==VARM | *c==VARU) if (c[1]==0 && c[2]<argAm) argUsed[c[2]]++;
+        if (*c==VARO | *c==VARM | *c==VARU) if (c[1]==0 && c[2]<argAm) argUsed[c[2]] = true;
         c = nextBC(c);
       }
       for (i32 i = 0; i < 6; i++) if (argUsed[i]) {
