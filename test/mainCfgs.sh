ffe9497157a130f1788694fcb9f1ad31d6fbe6ca
dzaima <dzaimagit@gmail.com>
2022-04-27 21:29:53 +0300

    remove old t=

--- test/mainCfgs.sh
+++ test/mainCfgs.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-make heapverify && echo 'heapverify:'   && ./BQN -M 1000 "$1/test/this.bqn" -noerr bytecode header identity literal namespace prim simple syntax token under undo unhead || exit
-make   rtverify && echo   'rtverify:'   && ./BQN -M 1000 "$1/test/this.bqn" || exit
-echo 'gcc:';make CC=gcc t='gcc_basic' c && ./BQN -M 1000 "$1/test/this.bqn" || exit
+make heapverify && echo 'heapverify:' && ./BQN -M 1000 "$1/test/this.bqn" -noerr bytecode header identity literal namespace prim simple syntax token under undo unhead || exit
+make   rtverify && echo   'rtverify:' && ./BQN -M 1000 "$1/test/this.bqn" || exit
+make CC=gcc c   && echo        'gcc:' && ./BQN -M 1000 "$1/test/this.bqn" || exit
 
