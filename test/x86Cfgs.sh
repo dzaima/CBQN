9c1cbdc99863b1da0116df61cd832137b196dc5c
dzaima <dzaimagit@gmail.com>
2022-08-10 02:35:57 +0300

    document expected fail for x86Cfgs.sh

--- test/x86Cfgs.sh
+++ test/x86Cfgs.sh
@@ -4,3 +4,4 @@ echo 'alljit+heapverify:'                           && ./BQN -M 1000 "$1/test/th
 echo 'singeli:';make o3n-singeli                    && ./BQN -M 1000 "$1/test/this.bqn" || exit
 echo 'singeli vfy:';make heapverifyn-singeli        && ./BQN -M 1000 "$1/test/this.bqn" -noerr bytecode header identity literal namespace prim simple syntax token under undo unhead || exit
 echo '32-bit:';make f='-DDEBUG -m32' single-c FFI=0 && ./BQN -M 1000 "$1/test/this.bqn" || exit
+echo '(expected one failed test: 0.3‿1.01‿π(⋆⁼⌜≡÷˜⌜○(⋆⁼))0‿2‿5.6‿∞)'
