608489aed1bf4e8036bfeb5bea36fa925a5afc77
dzaima <dzaimagit@gmail.com>
2022-09-24 19:30:10 +0300

    fix gcc singeli build

--- src/utils/mut.c
+++ src/utils/mut.c
@@ -203,13 +203,15 @@ DEF_G(void, copy, B,             (void* a, usz ms, B x, usz xs, usz l), ms, x, x
 
 #if SINGELI
   #include <xmmintrin.h>
-  #if __GNUC__ && !__clang__ // yay gcc
-    __m128i _mm_loadu_si32(void* p) {
+  #if __GNUC__ && !__clang__ // old gcc versions don't define _mm_loadu_si32 & _mm_storeu_si32
+    __m128i custom_loadu_si32(void* p) {
       return (__m128i) _mm_load_ss(p);
     }
-    void _mm_storeu_si32(void* p, __m128i x) {
+    void custom_storeu_si32(void* p, __m128i x) {
       _mm_store_ss(p, _mm_castsi128_ps(x));
     }
+    #define _mm_loadu_si32 custom_loadu_si32 
+    #define _mm_storeu_si32 custom_storeu_si32
   #endif
   
   #pragma GCC diagnostic push
