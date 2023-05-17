de421fa6eb155f4e30b73c200333e56fdd4f40de
dzaima <dzaimagit@gmail.com>
2023-05-17 17:07:20 +0300

    more test rearrangement

--- test/moreCfgs.sh
+++ test/moreCfgs.sh
@@ -12,7 +12,7 @@ build/build f='-DWARN_SLOW'           c && ./BQN -p 2+2 2> /dev/null   || exit
 build/build f='-DMM=0 -DENABLE_GC=0'  c && ./BQN -p 2+2 || exit
 build/build f='-DMM=1'                c && ./BQN -p 2+2 || exit
 build/build f='-DMM=2'         debug  c && ./BQN "$1/test/this.bqn" || exit
-build/build usz=64             debug  c && ./BQN "$1/test/this.bqn" || exit
+build/build usz=64 debug singeli=1 arch=generic c && ./BQN "$1/test/this.bqn" || exit
 build/build f='-DTYPED_ARITH=0'       c && ./BQN -p 2+2 || exit
 build/build f='-DFAKE_RUNTIME'        c && ./BQN -p 2+2 || exit
 build/build f='-DALL_R0'              c && ./BQN -p 2+2 || exit
@@ -26,7 +26,7 @@ build/build f='-DOBJ_COUNTER'         c && ./BQN -p 2+2 || exit
 build/build f='-DNO_RT'               c && ./BQN -p 2+2 || exit
 build/build f='-DNATIVE_COMPILER'     c && ./BQN -p 2+2 || exit
 build/build f='-DNATIVE_COMPILER -DONLY_NATIVE_COMP -DFORMATTER=0 -DNO_RT -DNO_EXPLAIN' c && ./BQN -p 2+2 || exit
-build/build f='-DLOG_GC' arch=generic singeli=1 c && ./BQN -p 2+2 || exit
+build/build f='-DLOG_GC'              c && ./BQN -p 2+2 || exit
 build/build f='-DUSE_PERF'            c && ./BQN -p 2+2 || exit
 build/build f='-DREPL_INTERRUPT=0'    c && ./BQN -p 2+2 || exit
 build/build f='-DREPL_INTERRUPT=1'    c && ./BQN -p 2+2 || exit
