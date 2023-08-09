00c454d37a5b0f1f2f68763e625e74d727fdd96b
Marshall Lochbaum <mwlochbaum@gmail.com>
2023-08-09 19:42:29 -0400

    Add generic-architecture versions of Singeli scans

--- src/builtins/radix.h
+++ src/builtins/radix.h
@@ -12,14 +12,14 @@
 #define RDX_SUM_4(T)  GRADE_UD(c1[0]=c2[0]=c3[0]=0;,) T s0=0, s1=0, s2=0, s3=0; for(usz j=0;j<256;j++) { RDX_PRE(0); RDX_PRE(1); RDX_PRE(2); RDX_PRE(3); }
 
 #if SINGELI_AVX2
-extern void (*const avx2_scan_pluswrap_u8)(uint8_t* v0,uint8_t* v1,uint64_t v2,uint8_t v3);
-extern void (*const avx2_scan_pluswrap_u32)(uint32_t* v0,uint32_t* v1,uint64_t v2,uint32_t v3);
-#define RADIX_SUM_1_u8   avx2_scan_pluswrap_u8 (c0,c0,  256,0);
-#define RADIX_SUM_1_u32  avx2_scan_pluswrap_u32(c0,c0,  256,0);
-#define RADIX_SUM_2_u8   avx2_scan_pluswrap_u8 (c0,c0,2*256,0);
-#define RADIX_SUM_2_u32  avx2_scan_pluswrap_u32(c0,c0,2*256,0);
-#define RADIX_SUM_4_u8   avx2_scan_pluswrap_u8 (c0,c0,4*256,0);
-#define RADIX_SUM_4_u32  avx2_scan_pluswrap_u32(c0,c0,4*256,0);
+extern void (*const si_scan_pluswrap_u8)(uint8_t* v0,uint8_t* v1,uint64_t v2,uint8_t v3);
+extern void (*const si_scan_pluswrap_u32)(uint32_t* v0,uint32_t* v1,uint64_t v2,uint32_t v3);
+#define RADIX_SUM_1_u8   si_scan_pluswrap_u8 (c0,c0,  256,0);
+#define RADIX_SUM_1_u32  si_scan_pluswrap_u32(c0,c0,  256,0);
+#define RADIX_SUM_2_u8   si_scan_pluswrap_u8 (c0,c0,2*256,0);
+#define RADIX_SUM_2_u32  si_scan_pluswrap_u32(c0,c0,2*256,0);
+#define RADIX_SUM_4_u8   si_scan_pluswrap_u8 (c0,c0,4*256,0);
+#define RADIX_SUM_4_u32  si_scan_pluswrap_u32(c0,c0,4*256,0);
 #else
 #define RADIX_SUM_1_u8   RDX_SUM_1(u8)
 #define RADIX_SUM_1_u32  RDX_SUM_1(u32)
@@ -30,9 +30,9 @@ extern void (*const avx2_scan_pluswrap_u32)(uint32_t* v0,uint32_t* v1,uint64_t v
 #endif
 
 #if SINGELI_AVX2 && !USZ_64
-#define RADIX_SUM_1_usz  avx2_scan_pluswrap_u32(c0,c0,  256,0);
-#define RADIX_SUM_2_usz  avx2_scan_pluswrap_u32(c0,c0,2*256,0);
-#define RADIX_SUM_4_usz  avx2_scan_pluswrap_u32(c0,c0,4*256,0);
+#define RADIX_SUM_1_usz  si_scan_pluswrap_u32(c0,c0,  256,0);
+#define RADIX_SUM_2_usz  si_scan_pluswrap_u32(c0,c0,2*256,0);
+#define RADIX_SUM_4_usz  si_scan_pluswrap_u32(c0,c0,4*256,0);
 #else
 #define RADIX_SUM_1_usz  RDX_SUM_1(usz)
 #define RADIX_SUM_2_usz  RDX_SUM_2(usz)
