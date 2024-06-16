a671ec9462f39cc5ee35897d04a1cf9f46fe5b50
dzaima <dzaimagit@gmail.com>
2024-06-16 13:58:50 +0300

    aarch64 bittab

--- src/utils/includeSingeli.h
+++ src/utils/includeSingeli.h
@@ -15,6 +15,14 @@
       #define _mm256_zextsi128_si256(X) _mm256_setr_m128i(X, _mm_setzero_si128())
     #endif
   #endif
+  #if defined(__aarch64__)
+    #define unpacked_vqtbl2q_s8(A,B,I)     vqtbl2q_s8((int8x16x2_t){A,B}, I)
+    #define unpacked_vqtbl3q_s8(A,B,C,I)   vqtbl3q_s8((int8x16x3_t){A,B,C}, I)
+    #define unpacked_vqtbl4q_s8(A,B,C,D,I) vqtbl4q_s8((int8x16x4_t){A,B,C,D}, I)
+    #define unpacked_vqtbl2q_u8(A,B,I)     vqtbl2q_u8((uint8x16x2_t){A,B}, I)
+    #define unpacked_vqtbl3q_u8(A,B,C,I)   vqtbl3q_u8((uint8x16x3_t){A,B,C}, I)
+    #define unpacked_vqtbl4q_u8(A,B,C,D,I) vqtbl4q_u8((uint8x16x4_t){A,B,C,D}, I)
+  #endif
   
   
   #define BCALL(N, X) N(b(X))
