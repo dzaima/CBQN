42b8420af621900aea68f0c94c47d7c078d30a0d
dzaima <dzaimagit@gmail.com>
2024-07-26 23:13:08 +0300

    ignore unused label warning

--- src/utils/includeSingeli.h
+++ src/utils/includeSingeli.h
@@ -37,6 +37,7 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-variable"
 #pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+#pragma GCC diagnostic ignored "-Wunused-label"
 #include SINGELI_FILE1(../../build/obj2/SINGELI_DIR/SINGELI_FILE.c)
 #pragma GCC diagnostic pop
 
