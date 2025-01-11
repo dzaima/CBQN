24bd6735bc85e72d54f3787bff3b26c7c36544d3
vylsaz <cokelover7244@proton.me>
2025-01-11 18:08:33 +0000

    Windows: use _wfullpath for realpath

--- src/windows/realpath.h
+++
@@ -1,7 +0,0 @@
-#ifndef REALPATH_H
-#define REALPATH_H
-
-char* realpath(const char *__restrict path, char *__restrict resolved_path);
-bool winIsAbsolute(const char* path);
-
-#endif /* REALPATH_H */
