f531e5a252d48ece8c4c55904f08a71f1a3f7ac3
dzaima <dzaimagit@gmail.com>
2025-11-26 14:56:50 +0200

    limit integer loadu/storeu polyfill to gcc≤10
    makes gcc ubsan not complain on misaligned pointers

--- src/utils/includeSingeli.h
+++ src/utils/includeSingeli.h
@@ -2,17 +2,19 @@
   #define SI_C_DEFS
   
   #if EXACTLY_GCC && (defined(__x86_64) || defined(__amd64__)) // old gcc versions don't define some smaller-size loads/stores
-    #include <xmmintrin.h>
-    static __m128i custom_loadu_si32(void* p) { return (__m128i) _mm_load_ss(p); }
-    static __m128i custom_loadu_si16(void* p) { return _mm_insert_epi16(_mm_setzero_si128(), *(i16*)p, 0); }
-    static void custom_storeu_si32(void* p, __m128i x) { _mm_store_ss(p, _mm_castsi128_ps(x)); }
-    static void custom_storeu_si16(void* p, __m128i x) { *(i16*)p = _mm_extract_epi16(x, 0); }
-    #define _mm_loadu_si16 custom_loadu_si16
-    #define _mm_loadu_si32 custom_loadu_si32
-    #define _mm_storeu_si16 custom_storeu_si16
-    #define _mm_storeu_si32 custom_storeu_si32
-    #if __GNUC__<=9
-      #define _mm256_zextsi128_si256(X) _mm256_setr_m128i(X, _mm_setzero_si128())
+    #if __GNUC__<=10
+      #include <xmmintrin.h>
+      static __m128i custom_loadu_si32(void* p) { return (__m128i) _mm_load_ss(p); }
+      static __m128i custom_loadu_si16(void* p) { return _mm_insert_epi16(_mm_setzero_si128(), *(i16*)p, 0); }
+      static void custom_storeu_si32(void* p, __m128i x) { _mm_store_ss(p, _mm_castsi128_ps(x)); }
+      static void custom_storeu_si16(void* p, __m128i x) { *(i16*)p = _mm_extract_epi16(x, 0); }
+      #define _mm_loadu_si16 custom_loadu_si16
+      #define _mm_loadu_si32 custom_loadu_si32
+      #define _mm_storeu_si16 custom_storeu_si16
+      #define _mm_storeu_si32 custom_storeu_si32
+      #if __GNUC__<=9
+        #define _mm256_zextsi128_si256(X) _mm256_setr_m128i(X, _mm_setzero_si128())
+      #endif
     #endif
   #endif
   #if defined(__aarch64__)
