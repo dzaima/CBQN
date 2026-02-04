dba93f5b18dfd786b4ad23c3012009f97ded949e
dzaima <dzaimagit@gmail.com>
2026-02-05 00:38:51 +0200

    heapverify builds now pass fill tests

--- test/mainCfgs.sh
+++ test/mainCfgs.sh
@@ -3,7 +3,7 @@ if [ "$#" -ne 1 ]; then
   echo "Usage: $0 path/to/mlochbaum/BQN"
   exit
 fi
-make heapverify && echo 'heapverify:' && ./BQN -M 1000 "$1/test/this.bqn" -noerr bytecode header identity literal namespace prim simple syntax token under undo unhead || exit
+make heapverify && echo 'heapverify:' && ./BQN -M 1000 "$1/test/this.bqn" -noerr || exit
 make   rtverify && echo   'rtverify:' && ./BQN -M 1000 "$1/test/this.bqn" || exit
 make CC=gcc c   && echo        'gcc:' && ./BQN -M 1000 "$1/test/this.bqn" || exit
 
--- test/x86Cfgs.sh
+++ test/x86Cfgs.sh
@@ -4,8 +4,8 @@ if [ "$#" -ne 1 ]; then
   exit
 fi
 make f='-DDEBUG -DHEAP_VERIFY -DUSE_SETJMP=0 -DJIT_START=0' single-c
-echo 'alljit+heapverify:'                           && ./BQN -M 1000 "$1/test/this.bqn" -noerr bytecode header identity literal namespace prim simple syntax token under undo unhead || exit
-echo 'native vfy:';make heapverifyn                 && ./BQN -M 1000 "$1/test/this.bqn" -noerr bytecode header identity literal namespace prim simple syntax token under undo unhead || exit
+echo 'alljit+heapverify:'                           && ./BQN -M 1000 "$1/test/this.bqn" -noerr || exit
+echo 'native vfy:';make heapverifyn                 && ./BQN -M 1000 "$1/test/this.bqn" -noerr || exit
 echo 'native:';make o3n                             && ./BQN -M 1000 "$1/test/this.bqn" || exit
 echo 'sse2:';build/build singeli native=0           && ./BQN -M 1000 "$1/test/this.bqn" || exit
 echo '32-bit:';make f='-DDEBUG -m32' single-c FFI=0 && ./BQN -M 1000 "$1/test/this.bqn" || exit
